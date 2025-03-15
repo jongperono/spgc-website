45bee63756ea1cb6d1388088c381afa813d85e37
jong <jongperono@gmail.com>
2025-03-15 17:05:02 +0800

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-8xl">
+      South Pacific Davao Golf Club
+      </h1>
+      
+    </div>
+  );
+}
